3a0bdcbf3641036955bc26e160b100e876658b19
ChenDimri6 <chendimri@mail.tau.ac.il>
2024-05-15 08:54:35 +0300

    app with css

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import QuizBoard from './components/QuizBoard/index';
+
+function App() {
+  return (
+    <div className="App">
+      <QuizBoard/>
+    </div>
+  );
+}
+
+export default App;
